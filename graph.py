22a5e1119a4aee52bbff6da123bab0f212528108
Roman Prokhorov <prokhorov.pn@ucu.edu.ua>
2024-11-16 19:37:15 +0200

    better OOP change

--- graph.py
+++ graph.py
@@ -2,13 +2,25 @@
 Graphs library
 """
 
-class UndirectedGraph:
-    adjacency_list = {}
+class Graph:
+    def __init__(self) -> None:
+        self.__adjacency_list = {}
 
-    def read_file(self) -> dict[int]:
+    def __convert_to_adjacency_list(self, vertices: list[tuple[int, int]]) -> dict[int]:
+        pass
+
+    def read_file(self) -> list[tuple[int, int]]:
         pass
 
     def write_file(self, pathname: str) -> bool:
+        pass
+
+    def get_adjacency_list(self) -> dict[int]:
+        return self.__adjacency_list
+
+class UndirectedGraph(Graph):
+
+    def __convert_to_adjacency_list(self, vertices: list[tuple[int, int]]) -> dict[int]:
         pass
 
     def search_component_connectivity(self, adjacency_list: dict[int]) -> list[int]:
@@ -20,13 +32,8 @@ class UndirectedGraph:
     def search_points_connectivity(self, adjacency_list: dict[int]) -> list[int]:
         pass
 
-class DirectedGraph:
-    adjacency_list = {}
-
-    def read_file(self) -> dict[int]:
-        pass
-
-    def write_file(self, pathname: str) -> bool:
+class DirectedGraph(Graph):
+    def __convert_to_adjacency_list(self, vertices: list[tuple[int, int]]) -> dict[int]:
         pass
 
     def search_component_strong_connectivity(self, adjacency_list: dict[int]) -> list[int]:

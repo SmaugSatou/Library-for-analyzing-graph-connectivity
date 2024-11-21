3e851929bebd0230e736f9fd894d01cdb1fc65b9
Roman Prokhorov <roma.smaug@gmail.com>
2024-11-21 17:16:37 +0200

    Demonstration demo code

--- main.py
+++ main.py
@@ -2,9 +2,44 @@
 Demonstration program
 """
 
-class Visualizer:
-    def __init__(self) -> None:
-        pass
+import argparse
+
+import graph as g
 
 def main():
-    pass
+    """
+    Demonstration main function
+    """
+    parser = argparse.ArgumentParser(description="Library for analyzing graph connectivity")
+    parser.add_argument("function", choices=[
+        "search-bridges",
+        "search-component-connectivity",
+        "search-points-connectivity",
+        "search-component-strong-connectivity"
+    ], help="Functions to perform")
+
+    parser.add_argument("graph_file", type=str, help="Path to the CSV file containing the vertices")
+    parser.add_argument("--directed", action="store_true", help="Define if the graph is directed")
+
+    args = parser.parse_args()
+
+    try:
+        adjacency_list = g.read_file(args.graph_file, args.directed)
+    except (FileNotFoundError, PermissionError, IOError) as err:
+        print(err)
+        return
+
+    result = ''
+
+    if args.function == "search-bridges":
+        result = g.search_bridges(adjacency_list)
+    elif args.function == "search-component-connectivity":
+        result = g.search_component_connectivity(adjacency_list)
+    elif args.function == "search-points-connectivity":
+        result = g.search_points_connectivity(adjacency_list)
+    elif args.function == "search-component-strong-connectivity":
+        result = g.search_component_strong_connectivity(adjacency_list)
+    else:
+        parser.error("Unknown command")
+
+    print(result)
